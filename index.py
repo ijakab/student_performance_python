4094b5effca18ead27e42d1b0dffda5ca342c2c4
Ivan Jakab <ivan.jakab@gauss.hr>
2020-01-18 16:15:36 +0100

    feat: route to serve html

--- index.py
+++ index.py
@@ -1,12 +1,20 @@
-from flask import request, jsonify, Flask
+from flask import request, jsonify, Flask, send_from_directory
 import models
 
 app = Flask('student_performance_server')
-@app.route('/', methods=['POST'])
-def create_task():
+
+
+@app.route('/predict', methods=['POST'])
+def predict():
     features = request.json.get('features')
     modelOutputs = models.predict_all_from_features(features, prep=True)
     return jsonify(modelOutputs), 200
+
+
+@app.route('/web', methods=['GET'])
+def serve_web():
+    return send_from_directory('web', 'index.html')
+
 
 
 app.run()

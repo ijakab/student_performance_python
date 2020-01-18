ec950d35ba00b4ca47d5d4b67a8c5a4569987c27
Ivan Jakab <ivan.jakab@gauss.hr>
2020-01-18 17:09:25 +0100

    feat: display charts on hardcoded data

--- web/script.js
+++ web/script.js
@@ -4,6 +4,7 @@ window.onload = function () {
     callBtn.addEventListener('click', async function () {
         let data = await callApi()
         console.log(data)
+        plot(data)
     })
 
     async function callApi() {
@@ -18,6 +19,50 @@ window.onload = function () {
                 'Content-Type': 'application/json'
             }
         })
-        return await res.json()
+        let json = await res.json()
+        for(let key of Object.keys(json)) {
+            json[key] *= 5
+            json[key] = json[key].toFixed(1)
+            json[key] = Number(json[key])
+        }
+        return json
+    }
+
+    function plot(models) {
+        var ctx = document.getElementById('myChart').getContext('2d');
+        new Chart(ctx, {
+            type: 'bar',
+            data: {
+                labels: Object.keys(models),
+                datasets: [{
+                    label: 'Model predictions',
+                    data: Object.values(models),
+                    backgroundColor: [
+                        'rgba(255, 99, 132, 0.2)',
+                        'rgba(54, 162, 235, 0.2)',
+                        'rgba(255, 206, 86, 0.2)',
+                        'rgba(75, 192, 192, 0.2)',
+                        'rgba(153, 102, 255, 0.2)',
+                    ],
+                    borderColor: [
+                        'rgba(255, 99, 132, 1)',
+                        'rgba(54, 162, 235, 1)',
+                        'rgba(255, 206, 86, 1)',
+                        'rgba(75, 192, 192, 1)',
+                        'rgba(153, 102, 255, 1)',
+                    ],
+                    borderWidth: 1
+                }]
+            },
+            options: {
+                scales: {
+                    yAxes: [{
+                        ticks: {
+                            beginAtZero: true
+                        }
+                    }]
+                }
+            }
+        });
     }
 }
